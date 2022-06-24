5756829cf9b424d756d36542d4e1b79ab62e49dc
carl <carl@localhost-live.lan>
2022-06-24 05:28:01 -0400

    Add New Task dialog functioning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import { Task } from './task/task';
+import { CdkDragDrop, DropListRef, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { TaskDialogData, TaskDialogResult } from './task-dialog/task-dialog.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-fire';
+  todo: Task[] = [
+    {
+      title: 'Buy milk', 
+      description: 'Go to store & buy milk'
+    },
+    {
+      title: 'Create Kanban app', 
+      description: 'Using Firebase and Angular create a kanban app!'
+    }
+  ];
+  inProgress: Task[]=[];
+  done: Task[]=[];
+
+
+  constructor(private dialog: MatDialog){
+
+  }
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      }
+    });
+    dialogRef.afterClosed()
+      .subscribe((result: TaskDialogResult) => this.todo.push(result.task));
+  }
+
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    
 }
+
+  drop(event: CdkDragDrop<Task[] | any>): void {
+    if(event.previousContainer === event.container){
+      return;
+    }
+    if(!event.container.data || !event.previousContainer.data){
+      return;
+    }
+
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+
+
+}
+
+
