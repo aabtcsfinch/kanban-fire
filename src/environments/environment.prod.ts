4114a316ef9455243edf500f29da61841174c7ea
carl <carl@localhost-live.lan>
2022-06-25 19:12:20 -0400

    Added and configured @angular/fire correctly to the correct Firebase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'kanban-fire-50128',
+    appId: '1:266100305054:web:79d544d6d5781b85c1df8f',
+    storageBucket: 'kanban-fire-50128.appspot.com',
+    apiKey: 'AIzaSyBzhdv6d4HLiVlw-oMur3rS02b3Mdo6b-E',
+    authDomain: 'kanban-fire-50128.firebaseapp.com',
+    messagingSenderId: '266100305054',
+    measurementId: 'G-F116DQHHXP',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'kanban-fire-50128',
+    appId: '1:266100305054:web:79d544d6d5781b85c1df8f',
+    storageBucket: 'kanban-fire-50128.appspot.com',
+    apiKey: 'AIzaSyBzhdv6d4HLiVlw-oMur3rS02b3Mdo6b-E',
+    authDomain: 'kanban-fire-50128.firebaseapp.com',
+    messagingSenderId: '266100305054',
+    measurementId: 'G-F116DQHHXP',
+  },
   production: false
 };
 
